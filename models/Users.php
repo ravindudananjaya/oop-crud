da26b7c2f29107af9be28076ae10fc5c6d605223
ravindudananjaya <ravindudananjaya@gmail.com>
2020-11-22 22:41:30 +0530

    new

--- models/Users.php
+++ models/Users.php
@@ -6,28 +6,18 @@ class Users extends DB{
 	public function login($payload)
 	{
 
-		// $allUsers = mysqli_query($this->db, "SELECT * FROM info");
-		// // $Users = mysqli_fetch_all($allUsers);
-		// // print_r($Users);
-
-		// // $username = ($payload['username']);
-
-		// while ($Users = mysqli_fetch_array($allUsers))  {
-		// 	$records[] = $Users;
-		// }
-
-		// print_r ($records); 
-
 
 		$username = ($payload['username']);
+		$password = ($payload['password']);
 
-		$sql = "SELECT * FROM users WHERE username='$username'";
+		$sql = "SELECT * FROM users WHERE username='$username' and password = '$password'";
 		$results = mysqli_query($this->db, $sql) or die( mysqli_error($this->db));
 		
-		// // $username = ($payload['username']);
-		// // $results = mysqli_query($this->db,  "SELECT * FROM users WHERE username=$username");
+	
 		$row = mysqli_fetch_array($results);
-		
+		if ($row == null) {
+			echo "Failed to login";
+		}
 		print_r($row);
 	
 	}
